12108d2f309be4f655a58fd4a07feae818a66b14
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-11-16 12:54:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
-// const array = [1, 3, 2, 4];
+const array = [1, 3, 2, 4];
 
-// const findNumber = function () {
-//   const array2 = [...array].sort();
-//   for (let i = 0; i < array2.length; i++) {
-//     if (array2[i + 1] - array2[i] > 1) {
-//       return array2[i] + 1;
-//     }
-//   }
-// };
-// console.log(findNumber(array));
-// //1,2,... n-2, n-1, n
+const findNumber = function () {
+  const array2 = [...array].sort();
+  for (let i = 0; i < array2.length; i++) {
+    if (array2[i + 1] - array2[i] > 1) {
+      return array2[i] + 1;
+    }
+  }
+};
+console.log(findNumber(array));
+//1,2,... n-2, n-1, n
 
-// const another = function () {
-//       const max = Math.max(array)
-//       const expectedSum =
+const another = function () {
+      const max = Math.max(array)
+      const expectedSum =
 
-// };
+};
 
-// const person = {
-//   name: "Arina",
-//   age: 24,
-//   surname: "AA",
-// };
+const person = {
+  name: "Arina",
+  age: 24,
+  surname: "AA",
+};
 
-// person.age = 88;
+person.age = 88;
 
-// const persons = [
-//   {
-//     name: "Arina",
-//     lastName: "Labuts",
-//     age: 24,
-//   },
-//   {
-//     name: "Nadiia",
-//     lastName: "Sorokova",
-//     age: 22,
-//   },
-//   {
-//     name: "Iryna",
-//     lastName: "Sorokova",
-//     age: 48,
-//   },
-//   {
-//     name: "Vlad",
-//     lastName: "Shevchuk",
-//     age: 28,
-//   },
-//   {
-//     name: "Sasha",
-//     lastName: "Labuts",
-//     age: 16,
-//   },
-//   {
-//     name: "Seva",
-//     lastName: "Labuts",
-//     age: 14,
-//   },
-//   {
-//     name: "Masha",
-//     lastName: "Lusyuk",
-//     age: 24,
-//   },
-// ];
+const persons = [
+  {
+    name: "Arina",
+    lastName: "Labuts",
+    age: 24,
+  },
+  {
+    name: "Nadiia",
+    lastName: "Sorokova",
+    age: 22,
+  },
+  {
+    name: "Iryna",
+    lastName: "Sorokova",
+    age: 48,
+  },
+  {
+    name: "Vlad",
+    lastName: "Shevchuk",
+    age: 28,
+  },
+  {
+    name: "Sasha",
+    lastName: "Labuts",
+    age: 16,
+  },
+  {
+    name: "Seva",
+    lastName: "Labuts",
+    age: 14,
+  },
+  {
+    name: "Masha",
+    lastName: "Lusyuk",
+    age: 24,
+  },
+];
 
 // function getLastNames() {
 //   const lastName = persons
