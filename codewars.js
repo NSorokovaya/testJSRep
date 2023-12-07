3c2124eefc3fbb9e02c72c333f4255aa6185dfe6
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-12-07 14:59:08 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,10 +1,10 @@
-// Training JS #9: loop statement --while and do..while
-// function padIt(str, n) {
-//   //coding here
-// }
-// 1;
-// function padIt(str, n) {
-//   2;
-//   //coding here
-//   3;
-// }
+// // Training JS #9: loop statement --while and do..while
+// // function padIt(str, n) {
+// //   //coding here
+// // }
+// // 1;
+// // function padIt(str, n) {
+// //   2;
+// //   //coding here
+// //   3;
+// // }
