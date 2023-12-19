0ddd412c55f66cd74cd6c2f16be6339bcbb65210
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-12-18 20:27:51 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -58,32 +58,7 @@ function whatNumberIsIt(n) {
     return "Input number is " + n;
   }
 }
-
-function whatNumberIsIt(n) {
-  if (isNaN(n)) {
-    return "Input number is Number.NaN";
-  } else if (n === Number.POSITIVE_INFINITY) {
-    return "Input number is Number.POSITIVE_INFINITY";
-  } else if (n === Number.NEGATIVE_INFINITY) {
-    return "Input number is Number.NEGATIVE_INFINITY";
-  } else if (n === Number.MAX_VALUE) {
-    return "Input number is Number.MAX_VALUE";
-  } else if (n === Number.MIN_VALUE) {
-    return "Input number is Number.MIN_VALUE";
-  } else if (typeof n === "number") {
-    return "Input number is " + n;
-  }
-}
 //Training JS #14: Methods of Number object--toString() and toLocaleString()
-function colorOf(r, g, b) {
-  //coding here
-  return (
-    "#" +
-    r.toString(16).padStart(2, "0") +
-    g.toString(16).padStart(2, "0") +
-    b.toString(16).padStart(2, "0")
-  );
-}
 function colorOf(r, g, b) {
   //coding here
   return (
