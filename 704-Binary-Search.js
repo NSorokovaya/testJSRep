2da004bfbe515f06e6d88739256e77ee526ce63c
NSorokovaya <nadiaasorokovaya@gmail.com>
2024-07-29 11:53:02 +0200

    added recursive solution for binary search

--- 704-Binary-Search.js
+++ 704-Binary-Search.js
@@ -20,4 +20,18 @@ var search = function (nums, target) {
   return -1;
 };
 
+// console.log(search(nums, target));
+
+//рекурсивный метод
+
+var search = function (nums, target, left = 0, right = nums.length - 1) {
+  let middleIndex = Math.ceil((left + right) / 2);
+  if (nums[middleIndex] === target) {
+    return middleIndex;
+  } else if (nums[middleIndex] < target) {
+    return search(nums, target, middleIndex, right);
+  }
+  return search(nums, target, left, middleIndex);
+};
+
 console.log(search(nums, target));
