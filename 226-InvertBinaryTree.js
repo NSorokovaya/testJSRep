b1bdb4fdd62a9de2f041d242a4d94e59b7aaf323
NSorokovaya <nadiaasorokovaya@gmail.com>
2024-08-02 13:43:18 +0200

    another version

--- 226-InvertBinaryTree.js
+++ 226-InvertBinaryTree.js
@@ -1,3 +1,4 @@
+//РЕКУРСИВНЫЙ
 var invertTree = function (root) {
   if (root === null) {
     return null;
@@ -8,5 +9,31 @@ var invertTree = function (root) {
   invertTree(root.left);
   invertTree(root.right);
 
+  return root;
+};
+
+//ИМПЕРАТИВНЫЙ(ИСПОЛЬЗУЯ ОЧЕРЕДЬ)
+
+var invertTree = function (root) {
+  if (root === null) {
+    return root;
+  }
+
+  let queue = [root];
+  console.log(queue);
+  while (queue.length) {
+    let node = queue.shift();
+    let left = node.left;
+    node.left = node.right;
+
+    node.right = left;
+
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+  }
   return root;
 };
